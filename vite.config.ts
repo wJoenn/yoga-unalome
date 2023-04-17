df8de5113a899da6e045f06e6dbe95a772987804
Louis Ramos <louisramosdev@gmail.com>
2023-04-17 23:31:12 +0200

    Fixed vite config lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from "vite"
 import FullReload from "vite-plugin-full-reload"
-import RubyPlugin from 'vite-plugin-ruby'
-import StimulusHMR from 'vite-plugin-stimulus-hmr'
+import RubyPlugin from "vite-plugin-ruby"
+import StimulusHMR from "vite-plugin-stimulus-hmr"
 
 export default defineConfig({
   clearScreen: false,
   plugins: [
-    RubyPlugin(), 
-    StimulusHMR(), 
-    FullReload(["config/routes.rb", "app/views/**/*"], {delay: 200}),
+    RubyPlugin(),
+    StimulusHMR(),
+    FullReload(["config/routes.rb", "app/views/**/*"], { delay: 200 }),
   ],
 })
