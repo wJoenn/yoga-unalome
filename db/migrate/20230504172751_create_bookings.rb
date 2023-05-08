0dec25607305c2eea6c737acdf8851527ca642fe
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-05-08 22:49:28 +0600

    updated the booking migration file, db:drop, db:create, db:migrate

--- db/migrate/20230504172751_create_bookings.rb
+++ db/migrate/20230504172751_create_bookings.rb
@@ -3,9 +3,10 @@ class CreateBookings < ActiveRecord::Migration[7.0]
     create_table :bookings do |t|
       t.references :session, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.boolean :canceled
+      t.boolean :canceled, null: false, default: false
 
       t.timestamps
     end
+    change_column :bookings, :canceled, :boolean, null: false, default: false
   end
 end
