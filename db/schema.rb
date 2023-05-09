f75e275083297038a742285af9fec9b2d8f32488
Louis Ramos <louisramosdev@gmail.com>
2023-05-09 21:30:57 +0200

    Updated db schema with duration instead of end_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_172751) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_184007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,10 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_172751) do
   create_table "sessions", force: :cascade do |t|
     t.string "address"
     t.datetime "start_time"
-    t.datetime "end_time"
     t.integer "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "duration"
   end
 
   create_table "users", force: :cascade do |t|
