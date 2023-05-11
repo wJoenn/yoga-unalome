0760e9236396dcb70240c6344ce5903f19f0279e
Louis Ramos <louisramosdev@gmail.com>
2023-05-11 17:34:37 +0200

    Forgot couple settings in initializer

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,4 +1,5 @@
 # encoding : utf-8
+Money.locale_backend = :i18n
 
 MoneyRails.configure do |config|
 
@@ -71,7 +72,7 @@ MoneyRails.configure do |config|
   #
   # set to BigDecimal::ROUND_HALF_EVEN by default
   #
-  # config.rounding_mode = BigDecimal::ROUND_HALF_UP
+  config.rounding_mode = BigDecimal::ROUND_HALF_UP
 
   # Set default money format globally.
   # Default value is nil meaning "ignore this option".
