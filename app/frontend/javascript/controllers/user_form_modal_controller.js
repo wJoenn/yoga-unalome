ba18c19b5e25712a9b8f5a67ab1b488d229ba176
Louis Ramos <louisramosdev@gmail.com>
2023-05-21 16:53:29 +0200

    Changed partial to devise session new page

--- app/frontend/javascript/controllers/user_form_modal_controller.js
+++ app/frontend/javascript/controllers/user_form_modal_controller.js
@@ -5,32 +5,14 @@ export default class extends Controller {
   static targets = ["modal", "form"]
 
   connect() {
-    this.html = document.querySelector("html")
-    this.body = document.querySelector("body")
+    window.addEventListener("keyup", event => {
+      if (event.key === "Escape") window.location.href = "/"
+    })
   }
 
   hideForm(event) {
     if (event.target === event.currentTarget) {
-      this.modalTarget.classList.add("hidden")
-
-      this.formTargets[0].classList.remove("hidden")
-      // this.formTargets[1].classList.add("hidden")
-
-      this.#toggleBodyScroll()
+      window.location.href = "/"
     }
-  }
-
-  toggleForm() {
-    this.formTargets.forEach(form => form.classList.toggle("hidden"))
-  }
-
-  showForm() {
-    this.modalTarget.classList.remove("hidden")
-    this.#toggleBodyScroll()
-  }
-
-  #toggleBodyScroll() {
-    this.html.classList.toggle("no-scroll")
-    this.body.classList.toggle("no-scroll")
   }
 }
