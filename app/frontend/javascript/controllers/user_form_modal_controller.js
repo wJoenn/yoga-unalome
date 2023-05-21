926e11a2c577215adf01982d24545d857a879217
Louis Ramos <louisramosdev@gmail.com>
2023-05-21 16:11:51 +0200

    Created button to toggle form

--- app/frontend/javascript/controllers/user_form_modal_controller.js
+++ app/frontend/javascript/controllers/user_form_modal_controller.js
@@ -2,15 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="user-form-modal"
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["modal", "form"]
 
   hideForm(event) {
     if (event.target === event.currentTarget) {
-      this.modalTarget.classList.remove("visible")
+      this.modalTarget.classList.add("hidden")
+      this.formTargets[0].classList.remove("hidden")
+      this.formTargets[1].classList.add("hidden")
     }
   }
 
+  toggleForm() {
+    this.formTargets.forEach(form => form.classList.toggle("hidden"))
+  }
+
   showForm() {
-    this.modalTarget.classList.add("visible")
+    this.modalTarget.classList.remove("hidden")
   }
 }
