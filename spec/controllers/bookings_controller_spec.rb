1824a616de4dbdffa46cab83f2a1061cb4b96169
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-05-25 20:48:36 +0200

    routes are updated / #create & #update methods are modified / RSpec missing

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -1,9 +1,8 @@
 require "rails_helper"
 
-RSpec.describe BookingsController, type: :request do
-  describe "#create" do
-    it "creates a new instance of Booking with proper params" do
-      params = { event: { start_time: 3.hours.from_now, duration: 90, address:, capacity: } }
-    end
-  end
-end
+# RSpec.describe BookingsController, type: :request do
+#   describe "#create" do
+#     it "creates a new instance of Booking" do
+#     end
+#   end
+# end
