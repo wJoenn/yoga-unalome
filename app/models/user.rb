1332c515e0572a5f5ed1c0fbb1ce9b49ac903ff4
Louis Ramos <louisramosdev@gmail.com>
2023-05-26 15:26:09 +0200

    Setup facebook auth with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
   before_save :capitalize_name, unless: :admin?
 
@@ -17,6 +17,19 @@ class User < ApplicationRecord
   def capitalize_name
     self.first_name = first_name.capitalize
     self.last_name = last_name.capitalize
+  end
+
+  def self.from_omniauth(auth)
+    first_name, *last_name = auth.info.name.split
+    email = auth.info.email
+    user = User.where(email:).first
+    return user if user
+
+    password = Devise.friendly_token[0, 20]
+    last_name = last_name.join(" ")
+    provider = auth.provider
+    uid = auth.uid
+    User.create!(email:, password:, first_name:, last_name:, provider:, uid:)
   end
 
   def full_name
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
 
   resources :events, only: %i[new create update destroy]
   get "user_dashboard/show", as: :dashboard
