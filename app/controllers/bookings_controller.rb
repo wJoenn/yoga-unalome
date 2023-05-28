cf15506302328ad2f704c79aaac50f937713d973
Louis Ramos <louisramosdev@gmail.com>
2023-05-28 17:09:40 +0200

    Fixed bug with locales in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,9 +7,9 @@ class BookingsController < ApplicationController
     @booking.user = current_user
 
     if @booking.save
-      redirect_to root_path, notice: I18n.t(".success")
+      redirect_to root_path, notice: t(".success")
     else
-      redirect_to root_path, alert: I18n.t(".error")
+      redirect_to root_path, alert: t(".error")
     end
   end
 
