6671ee0bba3ffe3b44841720e4fb085cd90e9810
Louis Ramos <louisramosdev@gmail.com>
2023-06-03 21:05:21 +0200

    Enabled gmail smtp for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,18 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "https://www.yogaunalome.com" }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV.fetch("GMAIL_ADDRESS"),
+    password: ENV.fetch("GMAIL_PASSWORD"),
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
