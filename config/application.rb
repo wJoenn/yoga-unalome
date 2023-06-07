bbbf96a1b0de059c81692efbbdfbe996afc434d0
Louis Ramos <louisramosdev@gmail.com>
2023-06-07 19:43:43 +0200

    Good Job background job handler installed

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module YogaUnalome
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_job.queue_adapter = :async
+    config.active_job.queue_adapter = :good_job
   end
 end
