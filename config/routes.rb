f04f7b2959ed96522b5008d973d650bb2218cc05
Louis Ramos <louisramosdev@gmail.com>
2023-06-07 20:10:35 +0200

    Mounted engine to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
+  authenticate :user, ->(user) { user.admin? } do
+    mount GoodJob::Engine => "good_job"
+  end
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     sessions: "users/sessions"
