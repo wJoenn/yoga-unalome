c0434c53eff2b6541120bb9ca94c735dbecaab75
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-06-13 12:00:40 +0600

    mobile media query with responsive navbar

--- app/frontend/javascript/controllers/navbar_toggle_controller.js
+++ app/frontend/javascript/controllers/navbar_toggle_controller.js
@@ -1,18 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["button", "links"]
+  static targets = ["links"]
 
-  connect() {
-    console.log("This is navbar_controller")
+  toggleNavbar() {
+    const navLinks = this.linksTarget
+    navLinks.classList.toggle("active")
   }
-
-  // toggleNavbar() {
-  //   const toggleButton = document.getElementsByClassName("toggle-button")[0]
-  //   const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
-  //   toggleButton.addEventListener("click", () => {
-  //     navbarLinks.classList.toggle("active")
-  //   })
-  // }
 }
