45871ca7366595bf466f8de41634dbc95467e717
Louis Ramos <louisramosdev@gmail.com>
2023-06-18 14:56:28 +0200

    UserDashboardController 100% coverage

--- spec/controllers/user_dashboard_controller_spec.rb
+++ spec/controllers/user_dashboard_controller_spec.rb
@@ -1,10 +1,36 @@
 require "rails_helper"
 
 RSpec.describe UserDashboardController, type: :request do
-  # describe "GET /show" do
-  #   it "returns http success" do
-  #     get "/user_dashboard/show"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  let!(:start_time) { 1.hour.from_now }
+  let!(:duration) { 90 }
+  let!(:address) { "Louvain La Neuve" }
+  let!(:capacity) { 9 }
+  let!(:price) { 12 }
+  let!(:title) { "Vinyasa Flow" }
+  let!(:event) { Event.create(start_time:, duration:, address:, capacity:, price:, title:) }
+
+  let(:password) { "password" }
+  let(:first_name) { "Chloe" }
+  let(:last_name) { "Aberg" }
+  let!(:user) { User.create(email: "info@yogaunalome.com", password:, first_name:, last_name:) }
+
+  describe "#authenticate_user!" do
+    it "requires authentification for #show" do
+      get dashboard_path
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe "#new" do
+    before do
+      sign_in user
+    end
+
+    it "stores the user's bookings in a variable" do
+      Booking.create(event:, user:)
+      get dashboard_path
+
+      expect(controller.view_assigns["bookings"].first).to be_a Booking
+    end
+  end
 end
